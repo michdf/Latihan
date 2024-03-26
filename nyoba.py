63e4c2f863fb59bdf5a96683de04c30079236e09
Micho Dhani Firmansyah <michodhani01@gmail.com>
2024-03-26 14:31:39 +0700

    uler🐍

---
+++ nyoba.py
@@ -0,0 +1,18 @@
+def Tambah(a,b):
+ return a + b
+
+def Kurang(a,b):
+ return a - b
+
+def kali(a,b):
+ return a * b
+
+def bagi(a,b):
+ return a/b
+
+if __name__ == "__main__":
+    print(tambah(7,8))
+    print(kurang(7,8))
+    print(kali(7,8))
+    print(bagi(72,8))
+
